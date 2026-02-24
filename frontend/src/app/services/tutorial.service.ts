5e4cd3affd6d9a6a1d69727105f125025f1348ad
Dinesh756 <nalliboinadinesh@gmail.com>
2026-02-24 21:05:34 +0530

    ip changed in url

--- frontend/src/app/services/tutorial.service.ts
+++ frontend/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://13.234.7.247:8080/api/tutorials';
+const baseUrl = 'http://3.110.95.55:8080/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
